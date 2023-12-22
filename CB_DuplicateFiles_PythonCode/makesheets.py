1dd7b24008753f1c6a6e05b3c337d7e7aee36d4b
emilyzhong <32210864+ezhon1@users.noreply.github.com>
2023-12-22 14:09:39 -0500

    Update makesheets.py

--- CB_DuplicateFiles_PythonCode/makesheets.py
+++ CB_DuplicateFiles_PythonCode/makesheets.py
@@ -3,17 +3,22 @@ import numpy as np
 from pandas.api.types import is_number
 from openpyxl import load_workbook
 
+# Importing spreadsheet and making dataframe
 df = pd.read_excel('CleanUpFileThing.xlsx', sheet_name='Duplicate Files')
 
 df = pd.DataFrame(df)
 
+# Gettign length of spreadsheet (number of rows)
 rows = len(df.index)
 
+# Getting al unique file owners
 owners = df['Owner'].unique()
 print (owners)
 
+# and number of unique owners
 numOwners = len(owners)
 
+# Making a new empty df for each unique owner and exporting to excel
 df2 = pd.DataFrame()
 ind=0
 for index in range(numOwners): 
